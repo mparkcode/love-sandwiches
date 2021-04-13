1df08282404f54f79358f065e350931d44dc89c0
Michael Park <mparkcode@gmail.com>
2021-04-13 06:34:06 +0000

    Add sales data to sales worksheet

--- run.py
+++ run.py
@@ -12,6 +12,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+
 def get_sales_data():
     """
     Get sales figures input from the user
@@ -23,13 +24,13 @@ def get_sales_data():
 
         data_str = input("Enter your data here: ")
         sales_data = data_str.split(',')
-        
 
         if validate_data(sales_data):
             print('Data is valid!')
             break
     return sales_data
-    
+
+
 def validate_data(values):
     """
     Inside the try, convers all string values into integers
@@ -45,9 +46,22 @@ def validate_data(values):
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
-    
+
     return True
 
+
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided
+    """
+    print("updating sales worksheet\n")
+    sales_worksheet = SHEET.worksheet('sales')
+    sales_worksheet.append_row(data)
+    print('sales worksheet updated successfully\n')
+
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
+
 
 
